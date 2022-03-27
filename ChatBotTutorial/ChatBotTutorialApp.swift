efb9b8dc1b4327b84c7a81638ff02364664771c3
waiga <hyang9387@gmail.com>
2022-03-27 18:00:38 +0900

    Initial Commit

---
+++ ChatBotTutorial/ChatBotTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotTutorialApp.swift
+//  ChatBotTutorial
+//
+//  Created by 김미향 on 2022/03/27.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatBotTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChatBotTutorial
+//
+//  Created by 김미향 on 2022/03/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
